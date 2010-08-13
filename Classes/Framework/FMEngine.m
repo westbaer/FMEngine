01521697b00359af490a6702737dee6061a231f5
Roman Shterenzon <roman.shterenzon@gmail.com>
2010-08-13 18:27:58 +0300

    Fix a memory leak

--- Classes/Framework/FMEngine.m
+++ Classes/Framework/FMEngine.m
@@ -55,9 +55,9 @@ static NSInteger sortAlpha(NSString *n1, NSString *n2, void *context) {
 		request = [NSURLRequest requestWithURL:dataURL];
 	} else {
 		#ifdef _USE_JSON_
-		request = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:[_LASTFM_BASEURL_ stringByAppendingString:@"?format=json"]]];
+		request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:[_LASTFM_BASEURL_ stringByAppendingString:@"?format=json"]]];
 		#else 
-		request = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:_LASTFM_BASEURL_]];
+		request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:_LASTFM_BASEURL_]];
 		#endif
 		[request setHTTPMethod:httpMethod];
 		[request setHTTPBody:[[self generatePOSTBodyFromDictionary:params] dataUsingEncoding:NSUTF8StringEncoding]];
@@ -101,9 +101,9 @@ static NSInteger sortAlpha(NSString *n1, NSString *n2, void *context) {
 		request = [NSURLRequest requestWithURL:dataURL];
 	} else {
 		#ifdef _USE_JSON_
-		request = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:[_LASTFM_BASEURL_ stringByAppendingString:@"?format=json"]]];
+		request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:[_LASTFM_BASEURL_ stringByAppendingString:@"?format=json"]]];
 		#else 
-		request = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:_LASTFM_BASEURL_]];
+		request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:_LASTFM_BASEURL_]];
 		#endif
 		
 		[request setHTTPMethod:httpMethod];
